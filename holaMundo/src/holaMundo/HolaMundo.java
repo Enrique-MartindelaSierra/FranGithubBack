3f4a2e6ea31d2a91dad39324230840ef0a7bcaa9
fran <ctefran@hotmail.com>
2023-01-17 13:02:04 +0100

    Hasta Long

--- holaMundo/src/holaMundo/HolaMundo.java
+++ holaMundo/src/holaMundo/HolaMundo.java
@@ -23,14 +23,33 @@ public class HolaMundo {
 	}
 	
 	private static void ejemplosTipos() {
+		// Número enteros
 		int numero = 5;
 		int numero2 = 7;
 		int suma = numero + numero2;
-		System.out.println(suma);
+		int resta = numero2 - numero;
+		double division = (double)numero2/numero;
+		System.out.println("La suma es: " + suma);
+		System.out.println("La resta es: " + resta);
+		System.out.println("La division es: " + division);
+		System.out.println("La division sobre la marcha es: " + ((double)numero2/numero));
+		// Número con decimales
+		double decimales1 = 3.14;
+		double decimales2 = 2.22;
+		System.out.println("La multiplicación es: "+ decimales1*decimales2);
+		System.out.println("La suma es: "+ (decimales1+decimales2));
+		double sumadecimales = numero + decimales1;
+		System.out.println("Suma con decimales es: " + sumadecimales);
+		// Enteros largos
+		int maxInt = Integer.MAX_VALUE; // 2147483647
+		int minInt = Integer.MIN_VALUE; // -2147483648
+		System.out.println("Suma de dos largos: " + (maxInt+minInt));
+		
 	}
 
 	public static void main(String[] args) {
-		ejemplosComentarios();
+		//ejemplosComentarios();
+		ejemplosTipos();
 	}
 
 }
