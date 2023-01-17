1bb133df40ff685978dac2f4f18304a4e38a8cf8
fran <ctefran@hotmail.com>
2023-01-17 12:26:12 +0100

    Tipos

--- holaMundo/src/holaMundo/HolaMundo.java
+++ holaMundo/src/holaMundo/HolaMundo.java
@@ -1,9 +1,36 @@
 package holaMundo;
 
+
+/**
+ * Esta es la clase inicial del curso
+ * @author Fran
+ * @since 1.0
+ */
 public class HolaMundo {
+	
+	public static void ejemplosComentarios() {
+		/*
+		 * Este es mi primer programa
+		 * Fran 17/01/2023
+		 */
+		// System.out.println("Hola Fran");
+		System.out.println("Adiós mundo cruel!");
+		System.out.println("Otra línea");
+		System.out.println("Tercera línea");
+		System.out.println("Tercera línea");
+		System.out.println("Segunda línea");
+		System.out.println("Arriba línea");
+	}
+	
+	private static void ejemplosTipos() {
+		int numero = 5;
+		int numero2 = 7;
+		int suma = numero + numero2;
+		System.out.println(suma);
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Adiós mundo cruel!");
+		ejemplosComentarios();
 	}
 
 }
