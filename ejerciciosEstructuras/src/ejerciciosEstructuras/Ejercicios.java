553defd516b91d10f0ca6fd2b75418c4e56e291b
fran <ctefran@hotmail.com>
2023-01-23 10:06:39 +0100

    Ejercicio12

--- ejerciciosEstructuras/src/ejerciciosEstructuras/Ejercicios.java
+++ ejerciciosEstructuras/src/ejerciciosEstructuras/Ejercicios.java
@@ -4,6 +4,8 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Ejercicios {
 		
@@ -225,6 +227,43 @@ public class Ejercicios {
 	}
 	
 	/**
+	 * 12. Crea una variable con el precio de una entrada que inicialmente valga 50. 
+	 * Pregunta al usuario su edad.
+	 * a. Si es menor de edad se le hará un descuento del 25% en la entrada.
+	 * b. Si es mayor de edad pero no está jubilado (65 años), pregúntale si es socio. Si el usuario
+	 * responde “sí” hazle un descuento del 40%.
+	 * c. Si está jubilado se le hará un descuento del 75%.
+	 * d. Finalmente muéstrale el precio que tiene que pagar por la entrada
+	 */
+	
+	public static void ejercicio12() {
+		final double MENOR_EDAD = 0.25;
+		final double DESCUENTO_SOCIO = 0.40;
+		final double DESCUENTO_JUBILADO = 0.75;
+		Scanner sc = new Scanner (System.in);  // Para leer datos por pantalla
+		double precioEntrada = 50;
+		System.out.println("Introduzca su edad:");
+		int edad = sc.nextInt();
+		sc.nextLine();
+		if(edad<18) {
+			precioEntrada = precioEntrada * (1-MENOR_EDAD);  // precio * 0,75
+		} else if(edad>=18 && edad<65) {
+			System.out.println("Es usted socio (sí/no):");
+			String respuesta = sc.nextLine();
+			// Solución con expresión regular		
+			Pattern regex = Pattern.compile("^s[iíÍ]?$", Pattern.CASE_INSENSITIVE);			
+			Matcher m = regex.matcher(respuesta);
+			if(m.matches()) {
+				precioEntrada = precioEntrada * (1 - DESCUENTO_SOCIO);
+			}
+		} else {
+			precioEntrada = precioEntrada * (1-DESCUENTO_JUBILADO);
+		}
+		System.out.println("Su precio es: " + precioEntrada);
+		sc.close();
+	}
+	
+	/**
 	 * Muestra por pantalla los números de 20 al 1 usando un bucle while
 	 */
 	public static void ejercicio13() {
@@ -267,9 +306,10 @@ public class Ejercicios {
 		//ejercicio4();
 		//ejercicio5();
 		//ejercicio9b();
+		ejercicio12();
 		//ejercicio13();
 		//ejercicio13CualquierNumero();
-		acaboCurso();
+		//acaboCurso();
 	}
 
 }
