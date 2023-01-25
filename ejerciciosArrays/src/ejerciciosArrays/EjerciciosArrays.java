8ba80714aa61ddc4bb6b5df9f512ba040ac8a689
fran <ctefran@hotmail.com>
2023-01-25 12:51:06 +0100

    Arrays Order Integer

--- ejerciciosArrays/src/ejerciciosArrays/EjerciciosArrays.java
+++ ejerciciosArrays/src/ejerciciosArrays/EjerciciosArrays.java
@@ -2,10 +2,16 @@ package ejerciciosArrays;
 
 public class EjerciciosArrays {
 	
+	public static void ejercicio1() {
+		String[] meses = {"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
+		int mesElegido = 7;
+		System.out.println(meses[mesElegido-1]);
+	}
+	
 	
 
 	public static void main(String[] args) {
-		
+		ejercicio1();
 
 	}
 
