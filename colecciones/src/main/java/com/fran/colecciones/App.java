f6f620cd9c35a69056f3f028a8bb1eeeac696caa
fran <ctefran@hotmail.com>
2023-02-15 13:18:12 +0100

    Ejercicio1

--- colecciones/src/main/java/com/fran/colecciones/App.java
+++ colecciones/src/main/java/com/fran/colecciones/App.java
@@ -3,6 +3,8 @@ package com.fran.colecciones;
 import java.security.KeyStore.Entry;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -113,11 +115,36 @@ public class App
 		System.out.println("El número de personas es: " + personas.size());  // 3		
 	}
 	
+	/**
+	 * 1. Crea una lista con los números 10, 20, 30 y 40. 
+	 * A continuación haz las siguientes
+	 * operaciones:
+	 * a. Añade los números 50 y 60 al final.
+	 * b. Borra el número 20.
+	 * c. Añade los valores 25 y 26 entre el 10 y el 30 (posición 1).
+	 * d. Dale la vuelta a la lista y muéstrala por pantalla
+	 * Los valores finales deberían ser: 60, 50, 30, 26, 25, 10
+	 */
+	public static void ejercicio1() {
+		List<Integer> numeros = new ArrayList<Integer>(Arrays.asList(10,20,30,40));
+		numeros.add(50);
+		numeros.add(60);
+		numeros.removeIf(e->e==20);
+		numeros.add(1, 25);
+		numeros.add(2, 26);
+		//numeros.sort(Comparator.reverseOrder());
+		//numeros.sort(Collections.reverseOrder());
+		Collections.reverse(numeros);  // No ordena, ni desordena, le da la vuelta
+		//numeros.sort((e1,e2)->e2-e1);  // Crearte tú mismo un comparador
+		numeros.forEach(e->System.out.println(e));
+		
+	}
 	
     public static void main( String[] args )
     {
     	//ejemplosListas();
     	//ejemploMap();
-    	ejemploSet();
+    	//ejemploSet();
+    	ejercicio1();
     }
 }
---
+++ ejercicioclases/src/main/java/com/fran/ejercicioclases/App.java
@@ -0,0 +1,13 @@
+package com.fran.ejercicioclases;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
