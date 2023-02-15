1f5508db839ea6bcad4dee942efdaacb5aeca8a4
fran <ctefran@hotmail.com>
2023-02-15 13:27:35 +0100

    Xml base

---
+++ xml/src/main/java/com/fran/xml/App.java
@@ -0,0 +1,13 @@
+package com.fran.xml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
