332a5178f73d5e5ca31fb9750068053eb9fecbad
fran <ctefran@hotmail.com>
2023-02-22 10:41:08 +0100

    ejemplo scanner

--- proyectoclases/src/main/java/com/fran/proyectoclases/App.java
+++ proyectoclases/src/main/java/com/fran/proyectoclases/App.java
@@ -1,17 +1,28 @@
 package com.fran.proyectoclases;
 
+import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 import com.fran.proyectoclases.entidades.Cuenta;
 import com.fran.proyectoclases.entidades.CuentaCaixa;
 import com.fran.proyectoclases.entidades.CuentaSabadell;
 import com.fran.proyectoclases.entidades.CuentaSantander;
 import com.fran.proyectoclases.enumerados.NivelCatalan;
+import com.fran.proyectoclases.utilidades.FicherosUtils;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
+	
+	private static Scanner sc;
+	private static List<Cuenta> cuentas = new ArrayList<Cuenta>();
+	private static List<CuentaCaixa> cuentasCaixa = new ArrayList<CuentaCaixa>();
+	
+	
+	public static void pruebas() {
         //Cuenta c = new Cuenta();
     	//CuentaSabadell cs = new CuentaSabadell();
     	CuentaCaixa cc = new CuentaCaixa();
@@ -29,7 +40,51 @@ public class App
     	System.out.println(cs2);
     	System.out.println(cs3);
     	System.out.println("El número de cuentas del santander es: " + CuentaSantander.getContador());
+	}
+	
+	public static void anyadirCuentasCaixa() {
+    	List<String> listaccstring = FicherosUtils.devolverLineasJava8(Paths.get("C:/ficheros/datosbancos/caixa.txt"));
     	
+    	for(String cuenta : listaccstring) {  // a partir de las líneas del fichero las añado a la lista de cuentas de la caixa
+    		String[] partes = cuenta.split(";");
+    		cuentasCaixa.add(new CuentaCaixa(partes[0],
+    				partes[1],
+    				LocalDate.parse(partes[2], DateTimeFormatter.ofPattern("dd/MM/yyyy")),
+    				partes[3],
+    				Double.parseDouble(partes[4]),
+    				NivelCatalan.Medio
+    				));
+    	}
+    	
+    	// probamos que la lista está rellena
+    	
+    	/*cuentasCaixa.stream()
+    	.filter(e->e.getSaldo()>30000)
+    	.forEach(e->System.out.println(e));*/
+    	
+    	cuentas.addAll(cuentasCaixa);  // esto añade todas las cuentas de la caixa dentro de la lista de cuentas
+ 
+	}
+	
+	public static void metodo1() {
+		System.out.println("Introduce valor: ");
+		int numero = Integer.parseInt(sc.nextLine());
+		System.out.println(numero);
+	}
+	
+	public static void metodo2() {
+		System.out.println("Introduce valor2: ");
+		int numero = Integer.parseInt(sc.nextLine());
+		System.out.println(numero);
+	}
+	
+    public static void main( String[] args )
+    {
+    	sc = new Scanner(System.in);
+    	anyadirCuentasCaixa();
+    	//metodo1();
+    	//metodo2();
 
+    	sc.close();
     }
 }
