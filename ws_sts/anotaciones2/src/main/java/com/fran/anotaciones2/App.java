ae239dea159c79d4c4df68227e3035ba36fff7a5
fran <ctefran@hotmail.com>
2023-03-16 13:01:23 +0100

    HibernateUtils save

--- ws_sts/anotaciones2/src/main/java/com/fran/anotaciones2/App.java
+++ ws_sts/anotaciones2/src/main/java/com/fran/anotaciones2/App.java
@@ -1,5 +1,9 @@
 package com.fran.anotaciones2;
 
+import java.util.HashSet;
+
+import com.fran.anotaciones2.entidades.Autores;
+import com.fran.anotaciones2.entidades.Libros;
 import com.fran.anotaciones2.utilidades.HibernateUtils;
 
 public class App 
@@ -7,14 +11,32 @@ public class App
 	
 	public static void mostrarLibros() {
 		HibernateUtils.openSession();
-		HibernateUtils.devolverListaObjetos("Libros").forEach(e->System.out.println(e));
+		HibernateUtils.devolverListaObjetos("Libros").forEach(e->System.out.println(e));  // Hibernate < 6.0
+		HibernateUtils.closeSession();
+	}
+	
+	public static void mostrarAutores() {
+		//HibernateUtils.openSession();
+		HibernateUtils.devolverListaObjetos(Autores.class).forEach(e->System.out.println(e)); // Hibernate >= 6.0
+		//HibernateUtils.closeSession();
+	}
+	
+	public static void anyadirAutor() {
+		if(HibernateUtils.save(new Autores("AAAAB","Nombre de prueba",new HashSet<Libros>(0)))) {
+			System.out.println("Autor añadido correctamente");
+		} else {
+			System.out.println("No se pudo añadir el autor");
+		}
 	}
 	
     public static void main( String[] args )
     {
     	java.util.logging.Logger.getLogger("org.hibernate").setLevel(java.util.logging.Level.SEVERE);
-    	mostrarLibros();
-    	
-    	
+    	//mostrarLibros();
+    	HibernateUtils.openSession();
+    	//mostrarAutores();
+    	anyadirAutor();
+    	mostrarAutores();
+    	HibernateUtils.closeSession();
     }
 }
--- ws_sts/anotaciones2/src/main/java/com/fran/anotaciones2/utilidades/HibernateUtils.java
+++ ws_sts/anotaciones2/src/main/java/com/fran/anotaciones2/utilidades/HibernateUtils.java
@@ -1,10 +1,15 @@
 package com.fran.anotaciones2.utilidades;
 
+import java.util.HashSet;
 import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+import com.fran.anotaciones2.entidades.Autores;
+import com.fran.anotaciones2.entidades.Libros;
 
 public class HibernateUtils {
 	
@@ -54,6 +59,7 @@ public class HibernateUtils {
 	/**
 	 * Dado el tipo de una clase, devuelve una lista con todos los elementos que tiene.
 	 * Usaremos este método en un proyecto >= Hibernate 6.0
+	 * <br> ex: HibernateUtils.devolverListaObjetos(Autores.class)
 	 * @param <T> Tipo de la clase
 	 * @param clase Clase pasada
 	 * @return Lista con el equivalente a todas las filas de la tabla de ese nombre
@@ -62,5 +68,23 @@ public class HibernateUtils {
 		return session.createSelectionQuery("from " + clase.getSimpleName(),clase).list();
 	}
 	
-
+	/**
+	 * Inserta el registro en su tabla correspondiente de la base de datos
+	 * <br> ex: HibernateUtils.save(new Autores("AAAAA","Nombre de prueba",new HashSet<Libros>(0)));
+	 * @param object Objeto de la clase que queremos insertar
+	 * @return True si la inserción es correcta. False si falla.
+	 */
+	public static boolean save(Object object) {
+		try {
+			Transaction trans = session.beginTransaction();  // abro la transacción		
+			session.persist(object);  // antiguo save. Persist es a partir de la versión 6.0
+			trans.commit();  // confirmo los cambios en la transacción
+			session.clear();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
 }
