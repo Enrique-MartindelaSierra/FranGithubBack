aa23d511cef28d713d0a8808187c1fb8734e0e6d
fran <ctefran@hotmail.com>
2023-03-21 13:53:35 +0100

    Params

--- ws_sts/ejemplo1/src/main/java/com/fran/ejemplo1/controllers/IndexController.java
+++ ws_sts/ejemplo1/src/main/java/com/fran/ejemplo1/controllers/IndexController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.fran.ejemplo1.entidades.Usuario;
@@ -37,7 +38,7 @@ public class IndexController {
 		return "index2";
 	}
 	
-	@GetMapping("/perfil")   // http://localhost:8080/app/index2
+	@GetMapping("/perfil")   // http://localhost:8080/app/perfil
 	public String perfil(Model model) {
 		model.addAttribute("titulo", "Perfil del Usuario");
 		//model.addAttribute("usuario",new Usuario("Fran","García","fran@iessanvicente.com"));
@@ -45,16 +46,26 @@ public class IndexController {
 		return "perfil";
 	}
 	
-	@GetMapping("/listar")   // http://localhost:8080/app/index2
+	@GetMapping("/listar")   // http://localhost:8080/app/listar
 	public String listar(Model model) {
+		/*List<Usuario> usuarios = new ArrayList<Usuario>();
+		usuarios.add(new Usuario("Fran","García","fran@iessanvicente.com"));
+		usuarios.add(new Usuario("Dani","García","dani@iessanvicente.com"));
+		usuarios.add(new Usuario("Paco","García","paco@iessanvicente.com"));*/
+		
+		model.addAttribute("titulo", "Lista de usuarios");
+		//model.addAttribute("usuarios",usuarios);
+		return "listar";
+	}
+	
+	
+	@ModelAttribute("usuarios")
+	public List<Usuario> poblarUsuarios(){
 		List<Usuario> usuarios = new ArrayList<Usuario>();
 		usuarios.add(new Usuario("Fran","García","fran@iessanvicente.com"));
 		usuarios.add(new Usuario("Dani","García","dani@iessanvicente.com"));
 		usuarios.add(new Usuario("Paco","García","paco@iessanvicente.com"));
-		
-		model.addAttribute("titulo", "Lista de usuarios");
-		model.addAttribute("usuarios",usuarios);
-		return "listar";
+		return usuarios;
 	}
 	
 
